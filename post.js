b0601094715f949c4c41df9331e0a1cf8167eef3
David Poindexter <davidpoindexter03@gmail.com>
2025-08-16 17:54:49 -0700

    function requireSignIn added to post, this new functionality requires that the user is signed in, in order to create a post.

--- post.js
+++ post.js
@@ -7,5 +7,23 @@ form.addEventListener("submit", function(e) {
 });
 
 function post(title, description, location, payType, pay) {
-    alert("Post: " + title + description + location + payType + pay);
+    if (sessionStorage.getItem("signedIn") == "true") {
+        alert("Post: " + title + description + location + payType + pay);
+    } else {
+        requireSignIn();
+    }
+}
+
+function requireSignIn() {
+    const ul = document.getElementById("post-ul");
+    const required = document.createElement("li");
+
+    required.innerHTML = "To create a post you must <a href='signin.html' class='Header-obj'>Sign In</a>";
+    required.style.fontSize = "16px";
+    required.style.textAlign = "center";
+    required.style.color = "red";
+    required.style.margin = "2%";
+
+    ul.children[10].style.marginTop = "0";
+    ul.insertBefore(required, ul.children[10]);
 }
