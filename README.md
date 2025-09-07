cf743c31ef5e926e623bf195ee0559cae38f13b2
David Poindexter <davidpoindexter03@gmail.com>
2025-09-06 18:04:53 -0700

    Post Added function, informs the user that their post was created, post.js event listener now resets the form, and some code cleanup.

--- README.md
+++ README.md
@@ -15,4 +15,5 @@ account test2 => name: Test Two, email: testtwo@gmail.com, password: testtwodavi
 post test1 => title: Post Test, description: this is a test for adding the post to the db, location: testing, type of pay: one-time, pay: 123
 post test2 => title: Post Test, description: this is a test for adding an additional post to the db, location: testing, type of pay: one-time, pay: 10
 post test3 => title: Post Test 2, description: this is a test for adding a post to the db, location: testing two, type of pay: hourly, pay: 10
-
+post test4 => title: Post Test 3, description: Adding another post, location: different, type of pay: yearly, pay: 100000
+post test5 => title: Post Test 4, description: Again, location: different, type: hourly, pay: 10
--- post.js
+++ post.js
@@ -3,16 +3,16 @@ const form = document.getElementById("post-form");
 form.addEventListener("submit", function(e) {
     e.preventDefault();
     const data = new FormData(this);
-    post(data.get("title"), data.get("description"), data.get("location"), data.get("payType"), data.get("pay"));
-});
 
-function post(title, description, location, payType, pay) {
     if (sessionStorage.getItem("signedIn") == "true") {
-        addPostToDB(title, description, location, payType, pay);
+        addPostToDB(data.get("title"), data.get("description"), data.get("location"), data.get("payType"), data.get("pay"));
+        e.target.reset(); 
+        postAdded();
     } else {
         requireSignIn();
     }
-}
+
+});
 
 function addPostToDB(ti, dsp, loc, pT, p) {
     const uID = sessionStorage.getItem('userId');
@@ -57,4 +57,18 @@ function requireSignIn() {
 
     ul.children[10].style.marginTop = "0";
     ul.insertBefore(required, ul.children[10]);
+}
+
+function postAdded() {
+    const ul = document.getElementById("post-ul");
+    const added = document.createElement("li");
+
+    added.innerHTML = "Post Created!";
+    added.style.fontSize = "16px";
+    added.style.textAlign = "center";
+    added.style.color = "rgb(21, 187, 21)";
+    added.style.margin = "2%";
+
+    ul.children[10].style.marginTop = "0";
+    ul.insertBefore(added, ul.children[10]);
 }
